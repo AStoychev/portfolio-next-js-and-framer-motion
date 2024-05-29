8ea8923237b1a50d977da1be6f2aaaf02ae8c60a
Stoychev <stoychev.nas@gmail.com>
2024-05-29 13:04:24 +0300

    Create popup Add links to contact Make animation to start only when user copy email field

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+"use client"
+import { useState } from "react";
+
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import Popup from "@/components/Popup";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
@@ -12,15 +16,30 @@ import { navItems } from "@/data";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
+    const [popup, setPopup] = useState<boolean>(false);
+    const [count, setCount] = useState<number>(0);
+
+    function handleClosePopup() {
+        setPopup(false)
+    };
+
+    const handleMouseOver = () => {
+        if (count <= 0) {
+            setPopup(true)
+            setCount(1)
+        }
+    };
+
     return (
         <main className="
         relative bg-black-100 flex justify-center
         items-center flex-col mx-auto
         sm:px-10 px-5 overflow-clip">
+            {popup && <Popup handleClosePopup={handleClosePopup} />}
             <div className="max-w-7xl w-full">
                 <FloatingNav navItems={navItems} />
                 <Hero />
-                <Grid />
+                <Grid handleMouseOver={handleMouseOver}/>
                 <RecentProjects />
                 <Clients />
                 <Experience />
